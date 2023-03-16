300820397c06f39830ccd1443e64a4d588da7084
rimjhim14 <124043778+rimjhim14@users.noreply.github.com>
2023-03-16 11:35:50 +0530

    Create interface.sv

---
+++ interface.sv
@@ -0,0 +1,11 @@
+interface adder_if(input logic clk, reset);
+  logic [3:0] a;
+  logic [3:0] b;
+  logic [4:0] y;
+endinterface
+/*
+interface add_if(input logic clk, reset);
+  logic [7:0] ip1, ip2;
+  logic [8:0] out;
+endinterface*/
+  
