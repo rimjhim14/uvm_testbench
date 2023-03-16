43b66566c6eea9010463e427953d2f338fec9308
rimjhim14 <124043778+rimjhim14@users.noreply.github.com>
2023-03-16 11:39:38 +0530

    Create package.sv

---
+++ package.sv
@@ -0,0 +1,13 @@
+package adder_pkg;
+   import uvm_pkg::*;
+   `include "uvm_macros.svh"
+   //`include "simpleadder_configuration.svh"
+   `include "seq_item.sv"
+   `include "sequence.sv"
+   `include "driver.sv" 
+   `include "monitor.sv"
+   `include "agent.sv"
+  // `include "simpleadder_scoreboard.svh"
+   `include "env.sv"
+   `include "test.sv"
+endpackage
